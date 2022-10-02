435979ebd3364b3130d6e17e6ea3e7f3361711a5
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-02 15:03:42 -0300

    feat: add global styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
-const App = (): JSX.Element => <RouterProvider router={router} />
+import { GlobalStyles } from './assets/styles/global'
+const App = (): JSX.Element => {
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
+  )
+}
 
 export default App
