53ab1bdd820a8727b47f4a516689847b96ebecf6
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-03 20:24:59 -0300

    feat: create helper handleChange

---
+++ src/helpers/handleChange.ts
@@ -0,0 +1,4 @@
+import React from 'react'
+export const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setState: React.Dispatch<React.SetStateAction<any>>): void => {
+  setState((data: any) => ({ ...data, [e.target.name]: e.target.value }))
+}
