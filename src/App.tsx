81a2595bf93de571e8671e2890465a159982e1df
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-02 21:17:19 -0300

    feat: implements ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 import { GlobalStyles } from './assets/styles/global'
+import { colors } from './assets/styles/colors'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors
+}
 const App = (): JSX.Element => {
   return (
-    <>
-      <GlobalStyles />
-      <RouterProvider router={router} />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <RouterProvider router={router} />
+      </>
+    </ThemeProvider>
+
   )
 }
 
