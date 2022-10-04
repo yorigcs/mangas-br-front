0de03eced7992aa6e2c2c18f7a69c97c1201b5a0
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-04 03:59:17 -0300

    feat: create sign-n page

---
+++ src/pages/sign-in/sign-in.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from '../../components/header/header'
+import { Main } from '../../components/main/main'
+import { Footer } from '../../components/Footer/footer'
+
+export const SignIn = (): JSX.Element => {
+  return (
+      <>
+        <Header />
+        <Main>
+        </Main>
+        <Footer />
+      </>
+  )
+}
