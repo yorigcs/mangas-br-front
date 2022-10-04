1b0b4a733028f508d3b424277fa787026cc892fc
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-04 03:43:29 -0300

    feat: add loading propriety to button

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -5,24 +5,26 @@ import styled from 'styled-components'
 interface Props {
   message: string
   onClick?: React.MouseEventHandler<HTMLButtonElement>
+  loading?: boolean
 }
-export const Button = ({ message, onClick }: Props): JSX.Element => {
+export const Button = ({ message, onClick, loading }: Props): JSX.Element => {
   return (
-        <ButtonDefault onClick={onClick}>
+        <ButtonDefault disabled={loading} loading={loading} onClick={onClick}>
             {message}
         </ButtonDefault>
   )
 }
-const ButtonDefault = styled.button`
-    cursor: pointer;
+const ButtonDefault = styled.button<{ loading?: boolean }>`
+    cursor: ${props => (props.loading ? 'wait' : 'pointer')};
     height: 48px;
     width: 80%;
     background-color: ${props => props.theme.colors.jellyBeanBlue};
+    opacity: ${props => (props.loading ? 0.7 : 1)};
     border: none;
     border-radius: 8px;
     color: ${props => props.theme.colors.Honeydew};
     font-size: 24px;
     &:hover {
-        opacity: 0.9;
+      opacity: ${props => (props.loading ? 0.7 : 0.9)};
     }
 `
