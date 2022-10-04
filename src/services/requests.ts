ca3628f01d4b3e51d2880c494f1be9ffc0cf477b
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-04 18:23:42 -0300

    feat: create singInRequest

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,6 +1,11 @@
 import api from './api'
 import { signUpData } from '../models/sign-up-model'
+import { signInData } from '../models/sign-in-model'
 
 export const singUpRequest = async (data: signUpData): Promise<any> => {
-  await api.post('sign-up', data)
+  return await api.post('sign-up', data)
+}
+
+export const singInRequest = async (data: signInData): Promise<any> => {
+  return await api.post('sign-in', data)
 }
