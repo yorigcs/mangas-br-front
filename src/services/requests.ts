eb25f5c0c5f2e65643b50e3a4c8da4ef443148f3
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-05 17:55:36 -0300

    feat: create SignInResponse interface

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,11 +1,18 @@
 import api from './api'
 import { signUpData } from '../models/sign-up-model'
 import { signInData } from '../models/sign-in-model'
-
+import { User } from '../models/user-model'
 export const singUpRequest = async (data: signUpData): Promise<any> => {
   return await api.post('sign-up', data)
 }
 
 export const singInRequest = async (data: signInData): Promise<any> => {
   return await api.post('sign-in', data)
+}
+
+export interface SignInResponse {
+  data: {
+    user: User
+    token: string
+  }
 }
