c1056cf0b6b8f7d5017fdb30d4d02fe1d0659b62
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-05 17:56:47 -0300

    refactor: change to calls signIn if sucess request

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -6,17 +6,18 @@ import { Input } from '../../components/inputs/Input'
 import { Button } from '../../components/buttons/button'
 
 import { signInData } from '../../models/sign-in-model'
-import { singInRequest } from '../../services/requests'
+import { singInRequest, SignInResponse } from '../../services/requests'
 import { handleChange } from '../../helpers'
 import { validateInputs } from './validateInputs'
 
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { MdEmail } from 'react-icons/md'
 import { RiLockPasswordFill } from 'react-icons/ri'
+import { useAuth } from '../../contexts/auth'
 
 export const SignIn = (): JSX.Element => {
-  const navigate = useNavigate()
+  const { signIn } = useAuth()
   const [userData, setUserData] = React.useState<signInData>(
     {
       email: '',
@@ -24,7 +25,7 @@ export const SignIn = (): JSX.Element => {
     }
   )
 
-  const [loading, setLoading] = React.useState(false)
+  const [loading, setLoading] = React.useState<boolean>(false)
 
   const [emailError, setEmailError] = React.useState<string | null>(null)
   const [passwordError, setPasswordError] = React.useState<string | null>(null)
@@ -37,11 +38,7 @@ export const SignIn = (): JSX.Element => {
     if (isFormValid) {
       setLoading(true)
       singInRequest(userData)
-        .then((resp) => {
-          const { data } = resp
-          console.log(data)
-          navigate('/')
-        })
+        .then((resp: SignInResponse) => { signIn(resp.data.user) })
         .catch((err) => { setRequestError(err.response.data.error) })
         .finally(() => setLoading(false))
     }
@@ -52,9 +49,9 @@ export const SignIn = (): JSX.Element => {
       <Header />
       <Main>
         <Form title='Fazer login' onSubmit={handleSubmit} err={requestError}>
-          <Input icon={<MdEmail />} placeHolder='Digite seu email...' name='email' type='email' onChange={e => handleChange(e, setUserData)} value={userData.email} err={emailError} loading={loading}/>
-          <Input icon={<RiLockPasswordFill />} placeHolder='Digite uma senha...' name='password' type='password' onChange={e => handleChange(e, setUserData)} value={userData.password} err={passwordError} loading={loading}/>
-          <Button message='Entrar' loading={loading}/>
+          <Input icon={<MdEmail />} placeHolder='Digite seu email...' name='email' type='email' onChange={e => handleChange(e, setUserData)} value={userData.email} err={emailError} loading={!!loading}/>
+          <Input icon={<RiLockPasswordFill />} placeHolder='Digite uma senha...' name='password' type='password' onChange={e => handleChange(e, setUserData)} value={userData.password} err={passwordError} loading={!!loading}/>
+          <Button message='Entrar' loading={!!loading}/>
           <Link to='/sign-up'>Não possui uma conta? Cadastre-se!</Link>
         </Form>
       </Main>
