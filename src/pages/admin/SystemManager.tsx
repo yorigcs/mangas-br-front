a1de4d735ebc3f8dc06d8d6ed65fb2611b8d2df7
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-06 18:50:45 -0300

    feat: create page system manager for admins

---
+++ src/pages/admin/SystemManager.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Footer } from '../../components/footer/Footer'
+import { Header } from '../../components/header/Header'
+import { Main } from '../../components/main/Main'
+
+export const SystemManger: React.FC = () => {
+  return (
+    <>
+    <Header />
+    <Main>
+    Página de admin
+    </Main>
+
+    <Footer />
+    </>
+  )
+}
