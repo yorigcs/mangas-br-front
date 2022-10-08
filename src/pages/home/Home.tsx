cbda98fe4645065485834efbf51aa926be954f67
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-08 19:46:22 -0300

    feat: add ContentBLocks to home screen

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,57 @@
-import React from 'react'
+import styled from 'styled-components'
+import { ContentBlock } from '../../components/contents/ContentBlock'
 import { Footer } from '../../components/footer/Footer'
 import { Header } from '../../components/header/Header'
 import { Main } from '../../components/main/Main'
 
-export const Home: React.FC = () => {
+export const Home = (): JSX.Element => {
   return (
     <>
-    <Header />
-    <Main>
+      <Header />
+      <Main>
+        <PrimaryContent>
+          <ContentBlock title='Bloco 1' size={{ height: '300px' }} >
+            oi
+          </ContentBlock>
 
-    </Main>
+          <ContentBlock title='Bloco 2' size={{ height: '1500px' }} >
+            oi
+          </ContentBlock>
 
-    <Footer />
+          <ContentBlock title='Bloco 2' size={{ height: '300px' }} >
+            oi
+          </ContentBlock>
+
+        </PrimaryContent>
+
+        <SecundaryContent>
+          <ContentBlock title='Bloco 4' size={{ height: '1500px' }} >
+            oi
+          </ContentBlock>
+
+        </SecundaryContent>
+
+      </Main>
+
+      <Footer />
     </>
   )
 }
+
+const PrimaryContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 800px;
+  @media only screen and (max-width: 480px) {
+   width:100%;
+  }
+
+  
+`
+const SecundaryContent = styled.div`
+  width: 400px;
+  @media only screen and (max-width: 480px) {
+   width:100%;
+  }
+`
