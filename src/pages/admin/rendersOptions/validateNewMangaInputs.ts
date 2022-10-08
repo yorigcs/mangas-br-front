a2f515729b3765763a3896462c2cf075c4820c60
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-08 19:51:49 -0300

    feat: create validation to creation of new mangas

---
+++ src/pages/admin/rendersOptions/validateNewMangaInputs.ts
@@ -0,0 +1,36 @@
+import { newMangaData } from '../../../models/mangaModels'
+
+interface SetError {
+  setNameError: React.Dispatch<React.SetStateAction<string | null>>
+  setCoverPictureError: React.Dispatch<React.SetStateAction<string | null>>
+  setDescriptionError: React.Dispatch<React.SetStateAction<string | null>>
+  setAuthorError: React.Dispatch<React.SetStateAction<string | null>>
+}
+export const validateNewMangaInputs = (mangaData: newMangaData, setError: SetError): boolean => {
+  let isValid = true
+  setError.setNameError(null)
+  setError.setCoverPictureError(null)
+  setError.setDescriptionError(null)
+  setError.setAuthorError(null)
+
+  const linkImageRegex = /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/
+  if (!linkImageRegex.test(mangaData.coverPicture)) {
+    setError.setCoverPictureError('Você deve fornecer um link válido!')
+    isValid = false
+  }
+
+  if (mangaData.name.length === 0) {
+    setError.setNameError('Você deve fornecer um nome para a obra!')
+    isValid = false
+  }
+  if (mangaData.description.length === 0) {
+    setError.setDescriptionError('Você deve fornecer um nome para a obra!')
+    isValid = false
+  }
+  if (mangaData.author.length === 0) {
+    setError.setAuthorError('Você deve fornecer um nome para a obra!')
+    isValid = false
+  }
+
+  return isValid
+}
