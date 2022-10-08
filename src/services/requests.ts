6109d823d999819b9073f07094e81c4e0b3da984
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-08 19:49:04 -0300

    feat: create new manga request

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -2,6 +2,15 @@ import api from './api'
 import { signUpData } from '../models/signUpModel'
 import { signInData } from '../models/signInModel'
 import { User } from '../models/userModel'
+import { newMangaData } from '../models/mangaModels'
+
+export interface SignInResponse {
+  data: {
+    user: User
+    token: string
+  }
+}
+
 export const singUpRequest = async (data: signUpData): Promise<any> => {
   return await api.post('sign-up', data)
 }
@@ -10,9 +19,6 @@ export const singInRequest = async (data: signInData): Promise<any> => {
   return await api.post('sign-in', data)
 }
 
-export interface SignInResponse {
-  data: {
-    user: User
-    token: string
-  }
+export const createNewMangaRequest = async (data: newMangaData): Promise<any> => {
+  return await api.post('create-manga', data)
 }
