3da97be41a160b5fe9b409900145f481a9c20701
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-08 19:53:55 -0300

    feat: create  handle Render options

--- src/pages/admin/SystemManager.tsx
+++ src/pages/admin/SystemManager.tsx
@@ -1,17 +1,69 @@
-import React from 'react'
+import styled from 'styled-components'
+
 import { Footer } from '../../components/footer/Footer'
 import { Header } from '../../components/header/Header'
 import { Main } from '../../components/main/Main'
+import { ContentBlock } from '../../components/contents/ContentBlock'
+import { MenuItem } from '../../components/header/user/menu/MenuItem'
+import { useState } from 'react'
+import { CreateNewManga } from './rendersOptions/CreateNewManga'
 
-export const SystemManger: React.FC = () => {
+export const SystemManger = (): JSX.Element => {
+  const [openOption, setOpenOption] = useState<string | null>(null)
+
+  const handleRenderOption = (): JSX.Element => {
+    switch (openOption) {
+      case 'createNewManga':
+        return <CreateNewManga />
+      default:
+        return <></>
+    }
+  }
   return (
     <>
-    <Header />
-    <Main>
-    Página de admin
-    </Main>
+      <Header />
+      <Main>
+        <PrimaryContent>
+          <ContentBlock title='Painel de administração' size={{ height: '300px' }} >
+            <MenuList>
+              <MenuItem onClick={() => setOpenOption('createNewManga')}>Criar nova obra</MenuItem>
+            </MenuList>
 
-    <Footer />
+          </ContentBlock>
+
+        </PrimaryContent>
+
+        <SecundaryContent>
+          {handleRenderOption()}
+        </SecundaryContent>
+
+      </Main>
+
+      <Footer />
     </>
   )
 }
+
+const MenuList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`
+
+const PrimaryContent = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 200px;
+  @media only screen and (max-width: 480px) {
+   width:100%;
+  }
+  
+`
+const SecundaryContent = styled.div`
+  width: 400px;
+  @media only screen and (max-width: 480px) {
+   width:100%;
+  }
+`
