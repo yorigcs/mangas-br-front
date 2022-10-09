2df6de0d64699c652a2a58b0584a993eea2ab9b7
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-09 16:47:35 -0300

    refactor: add handleRenderGenre to render with different type

--- src/pages/admin/rendersOptions/Genre.tsx
+++ src/pages/admin/rendersOptions/Genre.tsx
@@ -3,21 +3,47 @@ import { IoMdAddCircle, IoMdRemoveCircle } from 'react-icons/io'
 import { GenreClick } from '../../../models/genreModels'
 
 export const Genre = ({ id, name, onClick, type }: GenreClick): JSX.Element => {
-  return (
-    <GenreWrapper id={id}>
-      <Icon id={name} onClick={onClick} >{type === 'add' ? <IoMdAddCircle size={12} /> : <IoMdRemoveCircle size={12} />}</Icon>
-      {name}
-    </GenreWrapper>)
+  const handleRenderGenre = (): JSX.Element => {
+    switch (type) {
+      case 'add':
+        return (
+          <GenreWrapperWithIcon id={id}>
+            <Icon id={name} onClick={onClick} ><IoMdAddCircle size={12} /></Icon>
+            {name}
+          </GenreWrapperWithIcon>
+        )
+      case 'remove':
+        return (
+          <GenreWrapperWithIcon id={id}>
+            <Icon id={name} onClick={onClick} ><IoMdRemoveCircle size={12} /></Icon>
+            {name}
+          </GenreWrapperWithIcon>
+        )
+      default:
+        return (
+        <GenreWrapperDefault id={id}>
+          {name}
+        </GenreWrapperDefault>)
+    }
+  }
+  return (<>{handleRenderGenre()}</>)
 }
-const GenreWrapper = styled.div`
-    position: relative;
-    padding: 8px 24px 8px 8px;
+
+const GenreWrapperDefault = styled.div`
+    position: static;
+    padding: 8px;
     color: ${props => props.theme.colors.Honeydew};
     background-color: ${props => props.theme.colors.spaceCadet};
     width: fit-content;
     height: fit-content;
     border-radius: 8px;
+    border: solid 1px ${props => props.theme.colors.Honeydew};
   `
+
+const GenreWrapperWithIcon = styled(GenreWrapperDefault)`
+    position: relative;
+    padding-right: 24px;
+`
 
 const Icon = styled.div`
     position: absolute;
