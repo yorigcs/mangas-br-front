2cb54f849efdd62c864a795f4a59118681081f5d
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-09 16:48:51 -0300

    feat: create Manga and GenreManga interfaces

--- src/models/mangaModels.ts
+++ src/models/mangaModels.ts
@@ -5,3 +5,24 @@ export interface newMangaData {
   author: string
 
 }
+
+export interface Manga {
+  id: string
+  cover_picture: string
+  name: string
+  description: string
+  author: string
+  posted_by: string
+  followed_by: number
+  status: string
+  rating: number | null
+  created_at: string
+  updated_at: string
+  GenreManga: GenreManga[]
+}
+
+interface GenreManga {
+  genre_id: string
+  genre: { name: string }
+
+}
