ab36a6638a4ed1aca2cfffa36b0ac0143cd01ab6
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-09 21:38:03 -0300

    feat: create MangaWithChapter and Chapter interfaces

--- src/models/mangaModels.ts
+++ src/models/mangaModels.ts
@@ -24,5 +24,30 @@ export interface Manga {
 interface GenreManga {
   genre_id: string
   genre: { name: string }
+}
 
+export interface MangaWithChapter {
+  id: string
+  cover_picture: string
+  name: string
+  description: string
+  author: string
+  posted_by: string
+  followed_by: number
+  status: string
+  rating: number | null
+  created_at: string
+  updated_at: string
+  GenreManga: GenreManga[]
+  Chapter: Chapter[]
+}
+
+export interface Chapter {
+  id: string
+  name: string
+  chapter_num: number
+  manga_id: string
+  season: number
+  created_at: string
+  updated_at: string
 }
