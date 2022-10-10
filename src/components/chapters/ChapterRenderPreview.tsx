fbd8ca30ed87f815ae2568343af9cae1c4dc60d3
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-09 21:40:32 -0300

    feat: create ChapterRenderPreview

---
+++ src/components/chapters/ChapterRenderPreview.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+import { Chapter } from '../../models/mangaModels'
+import { convertToDateMessage } from '../../utils/dateFormater'
+
+export const ChapterRenderPreview = (props: Chapter): JSX.Element => {
+  return (
+          <ChapterWrapper id={props.id}>
+              <Title>Cápitulo {props.chapter_num}</Title>
+              <Time> {convertToDateMessage(props.updated_at)}</Time>
+          </ChapterWrapper>
+  )
+}
+
+const ChapterWrapper = styled.div`
+    cursor: pointer;
+    padding: 8px;
+    gap: 8px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 8px;
+    border: 1px solid ${props => props.theme.colors.crystal};;
+    background-color:${props => props.theme.colors.spaceCadet};
+    width:100%;
+    &:hover{
+        opacity: 0.9;
+    }
+    
+`
+
+const Title = styled.h2`
+  width:100%;
+  color: ${props => props.theme.colors.crystal}
+`
+const Time = styled.span`
+  width:100%;
+  font-size: 12px;
+  color: ${props => props.theme.colors.crystal}
+`
