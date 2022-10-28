2940772fe15d13eb21d99ae5c54a536d77aba847
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-27 23:55:56 -0300

    feat: create handleChangeFile

--- src/helpers/handleChange.ts
+++ src/helpers/handleChange.ts
@@ -2,3 +2,7 @@ import React from 'react'
 export const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setState: React.Dispatch<React.SetStateAction<any>>): void => {
   setState((data: any) => ({ ...data, [e.target.name]: e.target.value }))
 }
+
+export const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>, setState: React.Dispatch<React.SetStateAction<any>>): void => {
+  setState((data: any) => ({ ...data, [e.target.name]: e.target.files }))
+}
