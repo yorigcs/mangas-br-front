8e2ca54920ff7ba78460fda70be36ad9acca94b7
Yorí Galisteu <yorigcsdev@gmail.com>
2022-11-04 19:52:32 -0300

    refactor: change Form to handle msg according to status

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -1,24 +1,41 @@
 import styled from 'styled-components'
+import { Status } from '../../models/status'
 
 interface Props {
   children: React.ReactNode
   onSubmit?: React.FormEventHandler<HTMLFormElement>
   msg?: string | null
+  status?: Status
 }
-export const Form = ({ children, onSubmit, msg }: Props): JSX.Element => {
+export const Form = ({ children, onSubmit, msg, status }: Props): JSX.Element => {
+  const handleMsg = (): JSX.Element => {
+    switch (status) {
+      case 'sucess':
+        return <FormMsgSucess>{msg}</FormMsgSucess>
+      case 'error':
+        return <FormMsgError>{msg}</FormMsgError>
+      default:
+        return <></>
+    }
+  }
   return (
         <FormWrapper onSubmit={onSubmit}>
             {children}
-            {msg ? <FormMsg>{msg}</FormMsg> : null}
+            {msg ? handleMsg() : null}
         </FormWrapper>
   )
 }
 
-const FormMsg = styled.p`
+const FormMsgError = styled.p`
   color: ${props => props.theme.colors.desire};
   margin-top: 4px;
   font-weight: 700;
   font-size: 14px;
+`
+
+const FormMsgSucess = styled(FormMsgError)`
+  color: green
+
 `
 const FormWrapper = styled.form`
   display: flex;
