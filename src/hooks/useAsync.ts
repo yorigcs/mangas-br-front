14f1be619f2f95f247ccd5bcc996a0e6fa4e4203
Yorí Galisteu <yorigcsdev@gmail.com>
2022-11-04 19:49:24 -0300

    refactor: change act to throw error and add Status type

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,17 +1,18 @@
 import { useEffect, useState } from 'react'
+import { Status } from '../models/status'
 
 export const useAsync = <T>(handler: any, immediate: boolean = true):
 {
   data: T | null
-  status: 'loading' | 'sucess' | 'error' | null
+  status: Status
   errMsg: string | null
-  act: (...args: any) => Promise<string | T | null>
+  act: (...args: any) => Promise<Error | T | null>
 } => {
   const [data, setData] = useState<T | null>(null)
   const [errMsg, setErrMsg] = useState<null | string>(null)
   const [status, setStatus] = useState<'loading' | 'sucess' | 'error' | null>(null)
 
-  const act = async (...args: any): Promise<T | string | null> => {
+  const act = async (...args: any): Promise<T | Error | null> => {
     try {
       setStatus(status => (status = 'loading'))
       const res = await handler(...args)
@@ -21,7 +22,7 @@ export const useAsync = <T>(handler: any, immediate: boolean = true):
     } catch (err: any) {
       setErrMsg(error => (error = err.response.data.error))
       setStatus(status => (status = 'error'))
-      return errMsg
+      throw new Error(err.response.data.error)
     }
   }
 
