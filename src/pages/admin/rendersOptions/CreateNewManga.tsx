5dd7a3d21c64f12b25c4f8e7db47164d69e10f80
Yorí Galisteu <yorigcsdev@gmail.com>
2022-11-15 12:26:33 -0300

    refactor: change to createmangaExec

--- src/pages/admin/rendersOptions/CreateNewManga.tsx
+++ src/pages/admin/rendersOptions/CreateNewManga.tsx
@@ -15,7 +15,7 @@ import { validateNewMangaInputs } from './validateNewMangaInputs'
 
 export const CreateNewManga = (): JSX.Element => {
   const formData = new FormData()
-  const { data, act, errMsg, status } = useAsync<string>(async () => await createNewMangaRequest(formData), false)
+  const { data: dataMsg, act: createMangaExec, errMsg, status, resetStates } = useAsync<string>(async () => await createNewMangaRequest(formData), false)
   const [mangaData, setMangaData] = React.useState<newMangaData>(
     {
       name: '',
@@ -34,25 +34,28 @@ export const CreateNewManga = (): JSX.Element => {
     e.preventDefault()
 
     const isFormValid = validateNewMangaInputs(mangaData, { setNameError, setCoverPictureError, setDescriptionError, setAuthorError })
-    if (isFormValid) {
-      for (const file of mangaData.coverPicture) {
-        formData.append('images', file)
-      }
-      formData.append('name', mangaData.name)
-      formData.append('description', mangaData.description)
-      formData.append('author', mangaData.author)
+    if (!isFormValid) return
 
-      void act(formData)
+    for (const file of mangaData.coverPicture) {
+      formData.append('images', file)
     }
+    formData.append('name', mangaData.name)
+    formData.append('description', mangaData.description)
+    formData.append('author', mangaData.author)
+
+    createMangaExec(formData)
+      .then()
+      .catch((err) => { console.log(err.message) })
+      .finally(() => { resetStates(2000) })
   }
   return (
     <ContentBlock title='Criar novo manga' size={{ height: 'auto' }} >
-      <Form onSubmit={handleSubmit} msg={data ?? errMsg}>
+      <Form onSubmit={handleSubmit} status={status} msg={dataMsg ?? errMsg}>
         <Input icon={<BsChatSquareTextFill />} placeHolder='Digite o nome da obra...' name='name' type='text' onChange={e => handleChange(e, setMangaData)} value={mangaData.name} err={nameError} loading={status === 'loading'} />
         <Input icon={<AiFillFileImage />} name='coverPicture' type='file' onChange={e => handleChangeFile(e, setMangaData)} err={coverPictureError} loading={status === 'loading'} />
         <Input icon={<BsChatSquareTextFill />} placeHolder='Digite uma descrição...' name='description' type='text' onChange={e => handleChange(e, setMangaData)} value={mangaData.description} err={descriptionError} loading={status === 'loading'} />
         <Input icon={<FaUser />} placeHolder='Digite o nome do autor' name='author' type='text' onChange={e => handleChange(e, setMangaData)} value={mangaData.author} err={authorError} loading={status === 'loading'} />
-        <ButtonForm message='Criar Manga' />
+        <ButtonForm status={status} message='Criar Manga' />
       </Form>
     </ContentBlock>
   )
