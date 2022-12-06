11315437d0fcad07f1e156acd9e00529a5bb805e
Yorí Galisteu <yorigcsdev@gmail.com>
2022-12-05 21:40:19 -0300

    refactor: change logic to fetch manga data by mangaName

--- src/pages/manga/Manga.tsx
+++ src/pages/manga/Manga.tsx
@@ -1,37 +1,50 @@
-import { useLocation } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
+
 import { ChapterRenderList } from '../../components/chapters/ChapterList'
 import { ContentBlock } from '../../components/contents/ContentBlock'
+import { EmptyAnimationMessage } from '../../components/empty/EmptyAnimationMessage'
 import { Footer } from '../../components/footer/Footer'
 import { Header } from '../../components/header/Header'
 import { Main } from '../../components/main/Main'
 import { MangaInfo } from '../../components/manga/MangaInfo'
+
+import { useAsync } from '../../hooks/useAsync'
 import { MangaWithChapter } from '../../models/mangaModels'
+import { findMangaWithChaptersByName } from '../../services/requests'
 
 export const Manga = (): JSX.Element => {
-  const mangaWithChapter = useLocation().state as MangaWithChapter
-  const { Chapter, ...manga } = mangaWithChapter
+  const { mangaName } = useParams()
+  if (!mangaName) return <></>
+  const request = async (): Promise<MangaWithChapter> => (await findMangaWithChaptersByName(mangaName))
+  const { data: manga, status } = useAsync<MangaWithChapter>(request)
+
+  const handleRender = (): JSX.Element => {
+    if (!manga && status === 'loading') {
+      return (<>Carregando....</>)
+    } else if (manga) {
+      return (
+        <ContentBlock gap='30px' title={manga.name} size={{ height: 'auto' }} >
+          <MangaInfo {...manga} />
+          <ChapterRenderList chapters={manga.Chapter} />
+        </ContentBlock>
+      )
+    } else {
+      return (
+        <ContentBlock size={{ height: 'auto' }} >
+          <EmptyAnimationMessage>Não encontramos esse manga</EmptyAnimationMessage>
+        </ContentBlock>
+      )
+    }
+  }
   return (
     <>
       <Header />
       <Main>
         <PrimaryContent>
-          <ContentBlock gap='30px' title={manga.name} size={{ height: 'auto' }} >
-            <MangaInfo {...manga} />
-            <ChapterRenderList chapters={Chapter}/>
-          </ContentBlock>
-
+          {handleRender()}
         </PrimaryContent>
-
-        <SecundaryContent>
-          <ContentBlock title='Bloco 4' size={{ height: '1500px' }} >
-            oi
-          </ContentBlock>
-
-        </SecundaryContent>
-
       </Main>
-
       <Footer />
     </>
   )
@@ -50,13 +63,4 @@ const PrimaryContent = styled.div`
   }
 
   
-`
-const SecundaryContent = styled.div`
-  width: 400px;
-  @media only screen and (max-width: 480px) {
-   width:100%;
-  }
-  @media only screen and (max-width: 768px) {
-   width:100%;
-  }
 `
