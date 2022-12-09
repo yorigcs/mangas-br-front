6b3a127c4bc78666c1f8637843787c256519b559
Yorí Galisteu <yorigcsdev@gmail.com>
2022-12-09 14:30:04 -0300

    feat: create interface Page

--- src/models/pagesModel.ts
+++ src/models/pagesModel.ts
@@ -2,3 +2,14 @@ export interface AddPagesToChapter {
   chapter_id: string
   pagesImages: any
 }
+
+export interface Page {
+  id: string
+  name: string
+  page_img: string
+  page_num: number
+  chapter_id: string
+  created_at: Date
+  updated_at: Date
+
+}
